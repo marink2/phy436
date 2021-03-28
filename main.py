412d4b55804d156fd7f7bf895393e641143827c8
marink2 <marink2@tcnj.edu>
2021-03-28 09:58:59 -0400

    initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+    Author: Kevin Marin
+    Email:  marink2@tcnj.edu
+
+    This file executes the program
+
+"""
+
+
+from CrystalDynamics import CrystalDynamics as cd
+
+end = False
+while end:
+    print("Hello World")
