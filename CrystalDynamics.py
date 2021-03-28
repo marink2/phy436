562487a0b5fcfaed11d57068ab8fc24e4e9116da
marink2 <marink2@tcnj.edu>
2021-03-28 09:59:49 -0400

    initial files

---
+++ CrystalDynamics.py
@@ -0,0 +1,27 @@
+"""
+    Author: Kevin Marin
+    Email:  marink2@tcnj.edu
+
+    This file contains the set up for the interactive simulation.
+
+"""
+
+
+import numpy as np
+import vpython as vp
+
+
+class CrystalDynamics:
+
+    def __init__(self):
+        """
+        Constructor creates the scene for the interactive simulation.
+        """
+
+    def background(self):
+
+        return
+
+    def simulation(self):
+
+        return
